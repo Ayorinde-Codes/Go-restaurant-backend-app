49c01ef9b217102e6cd43b2b3f532df671817fa5
Ayorinde Akindemowo <ayorindeakindemowo@Ayorindes-MacBook-Pro.local>
2024-09-25 02:39:03 +0100

    setup models

--- controllers/orderItemsController.go
+++ controllers/orderItemsController.go
@@ -1,6 +1,9 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 func GetOrderItems() gin.HandlerFunc {
 	return func(c *gin.Context) {
@@ -15,6 +18,10 @@ func GetOrderItem() gin.HandlerFunc {
 func GetOrderItemsByOrder() gin.HandlerFunc {
 	return func(c *gin.Context) {
 	}
+}
+
+func ItemsByOrder(id string) (OrderItems []primitive.M, err error) {
+
 }
 
 func CreateOrderItem() gin.HandlerFunc {
