f987bfcaaede94d7dc0259aa870e72f5c58f418a
Ayorinde Akindemowo <ayorindeakindemowo@Ayorindes-MacBook-Pro.local>
2024-10-10 23:45:06 +0100

    get and create order items

--- controllers/orderItemsController.go
+++ controllers/orderItemsController.go
@@ -3,6 +3,7 @@ package controller
 import (
 	"context"
 	"golang-restaurant-backend-app/database"
+	"golang-restaurant-backend-app/models"
 	"log"
 	"net/http"
 	"time"
@@ -13,7 +14,7 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
-var OrderItemPack struct {
+type OrderItemPack struct {
 	Table_id    *string
 	Order_items []models.OrderItem
 }
@@ -45,6 +46,20 @@ func GetOrderItems() gin.HandlerFunc {
 
 func GetOrderItem() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		orderItemId := c.Param("order_item_id")
+		var orderItem models.OrderItem
+
+		err := OrderItemCollection.FindOne(ctx, bson.M{"orderItem_id": orderItemId}).Decode(&orderItem)
+
+		defer cancel()
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error occured while listing ordered items"})
+			return
+		}
+
+		c.JSON(http.StatusOK, orderItem)
 	}
 }
 
@@ -69,6 +84,52 @@ func ItemsByOrder(id string) (OrderItems []primitive.M, err error) {
 
 func CreateOrderItem() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		// Initialize the orderItemPack and order
+		var orderItemPack OrderItemPack
+		var order models.Order
+
+		// Bind JSON data to orderItemPack
+		if err := c.BindJSON(&orderItemPack); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request"})
+			return
+		}
+
+		order.Order_date = time.Now()
+
+		orderItemsToBeInserted := []interface{}{}
+		order.Table_id = orderItemPack.Table_id
+		order_id := OrderItemOrderCreated(order)
+
+		for _, orderItem := range orderItemPack.Order_items {
+			orderItem.Order_id = order_id
+
+			if validatorErr := validate.Struct(orderItem); validatorErr != nil {
+				c.JSON(http.StatusBadRequest, gin.H{"error": validatorErr.Error()})
+				return
+			}
+
+			// Populate order item fields
+			orderItem.ID = primitive.NewObjectID()
+			orderItem.Created_at = time.Now()
+			orderItem.Updated_at = time.Now()
+			orderItem.Order_item_id = orderItem.ID.Hex()
+			var num = toFixed(*orderItem.Unit_price, 2)
+			orderItem.Unit_price = &num
+
+			orderItemsToBeInserted = append(orderItemsToBeInserted, orderItem)
+		}
+
+		// Insert order items into MongoDB
+		insertedOrderItems, err := OrderItemCollection.InsertMany(ctx, orderItemsToBeInserted)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error inserting order items"})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"insertedOrderItems": insertedOrderItems.InsertedIDs})
 	}
 }
 
